466e2cc9aa9950e5857fee0890546c901c57363a
ALLEN-PC\Aallen <gaopu198778@163.com>
2017-07-18 10:38:12 +0800

    页面跳转渐隐渐现

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-	<router-view></router-view>
+	<transition name="router-fade" mode="out-in">
+		<router-view></router-view>
+	</transition>
 </template>
 
 <script>
@@ -8,5 +10,11 @@ export default {
 }
 </script>
 <style scoped>
-  @import './style/global.css'; 
+  @import './style/global.css';
+  .router-fade-enter-active, .router-fade-leave-active {
+	  	transition: opacity .3s;
+	}
+	.router-fade-enter, .router-fade-leave-active {
+	  	opacity: 0;
+	}
 </style>
